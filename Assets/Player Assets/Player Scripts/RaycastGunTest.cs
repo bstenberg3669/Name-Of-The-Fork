bf234647c624cf73aadc84c099ad42df177bee69
bstenberg3669 <bstenberg3669@student.pps.net>
2025-02-12 11:40:52 -0800

    Made animation for firing weapon. (working on making muzzle flash)

--- Assets/Player Assets/Player Scripts/RaycastGunTest.cs
+++ Assets/Player Assets/Player Scripts/RaycastGunTest.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class RaycastGunTest : MonoBehaviour
 {
-    public float range = 100000000f;
-    public float fireRate = 0.1f;
+    private Animator anim;
+    
+    private float range = 100000000f; //How far the gun can shoot (we don't have a range cap so don't change it)
 
-    public Transform attackPoint;
+    public Transform attackPoint; //Point that the gun shoots from
+    public ParticleSystem muzzleFlash; //Guess Einstein
+    
+    public float fireRate = 0.1f; //How long until the weapon can fire again
 
-    private float fireTimer;
+    private float fireTimer; //Counts time for fireRate to work
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -30,6 +35,8 @@ public class RaycastGunTest : MonoBehaviour
         }
     }
 
+    
+
     private void Fire()
     {
         if (fireTimer < fireRate) return;
@@ -40,7 +47,10 @@ public class RaycastGunTest : MonoBehaviour
         {
             Debug.Log(hit.collider.name);
         }
+        
+        anim.CrossFadeInFixedTime("Fire", 0.01f); //Plays Shooting Animation
+        muzzleFlash.Play();
 
-        fireTimer = 0.0f; //Reset timer
+        fireTimer = -0.1f; //Reset timer
     }
 }
